44c082410ebdb49ae7a07104c9d6139d3f77da67
MahadiMunna <mahadimunna.official@gmail.com>
2022-11-28 16:45:46 +0600

    isSeller checker added

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ async function run (){
             const user = await userCollection.findOne(query);
             res.send({isAdmin:user?.role==='admin'})
         })
+        app.get('/users/seller/:email',async(req,res)=>{
+            const email = req.params.email;
+            const query = {email}
+            const user = await userCollection.findOne(query);
+            res.send({isSeller:user?.role==='Seller'})
+        })
         app.post('/users',async(req,res)=>{
             const user = req.body;
             const result = userCollection.insertOne(user);
